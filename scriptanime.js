77822806c2f529eeb81ad66a0efb0d86cd686d44
abouquin <14873704+abouquin@users.noreply.github.com>
2025-04-18 17:58:31 +0900

    added grid svg

---
+++ scriptanime.js
@@ -0,0 +1,20 @@
+import {
+    animate,
+    stagger,
+    utils,
+    onScroll,
+    createTimer,
+    createTimeline
+} from './animejs/lib/anime.esm.js';
+
+console.log(document.querySelector('.svg-grid-bg'));
+
+animate('.svg-grid-bg', {
+    translateY: [-10, 10],
+    opacity: [0, 0.2],
+    duration: 4000,
+    delay: stagger(100),
+    alternate: true,
+    easing: 'easeInOutSine',
+    loop: true,
+});
